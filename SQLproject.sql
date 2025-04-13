ba7db287031d564f152006380f00f5d91121496d
Talha Türkoğlu <158575414+talhaturkoqlu61@users.noreply.github.com>
2025-04-13 16:48:57 +0300

    Create SQLproject.sql

---
+++ SQLproject.sql
@@ -0,0 +1,11 @@
+--SELECT city,country FROM country
+--INNER JOIN city ON city.country_id=country.country_id;
+--(1.QUESTION)
+
+--SELECT payment_id,first_name,last_name FROM customer
+--INNER JOIN payment on customer.customer_id=payment.customer_id;
+--(2.QUESTION)
+
+--SELECT rental_id,first_name,last_name FROM rental
+--INNER JOIN customer ON customer.customer_id=rental.customer_id;
+--(3.QUESTION)
